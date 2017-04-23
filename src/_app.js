e2c6e3aa5d38d65b375bbc119ab560e6d70f3eca
jB <jeremy.bates@gmail.com>
2017-04-23 12:38:23 -0400

    core vs components

--- src/_app.js
+++ src/_app.js
@@ -1,5 +1,5 @@
+require('./core/_js_global/utilLib/blazyInit');
+
 require('./components/accordion/accordion.js');
 require('./components/tabs/tabs.js');
 require('./components/navMain/navMain');
-
-require('./js_global/utilLib/blazyInit');
