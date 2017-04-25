5700f0be5a9890dd3d3331c7023b4d164d24e479
jB <jeremy.bates@gmail.com>
2017-04-25 11:57:17 -0400

    automating pattern library

--- _patternLib.js
+++ _patternLib.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const pathToComponents = 'src/components/';
 var componentsList = fs.readdirSync(pathToComponents, 'utf8');
 
+//instantiate
+var ObjectOfHTMLtoWrite = [];
+
 var indexOfDsStore = componentsList.indexOf('.DS_Store');
 //slice uses different index
 var componentsListMinusDSstore = componentsList.slice(indexOfDsStore + 1);
@@ -15,34 +18,44 @@ componentsListMinusDSstore.map(function(component){
 function writeThisFileName(component){
 	console.log(component);
 	//only add to array if there is an hbs file in a folder
-	const htmlElementToCreate = document.createElement('html');
-	const componentHTML = '{{> ../../components/' + component + '/' + component + '.hbs}}';
-
-	ObjectOfHTMLtoWrite.push(componentHTML)
+	const testHTML = '<section class="patternLib__section">{{> ../../components/' + component + '/' + component + '.hbs}}</section>';
+	ObjectOfHTMLtoWrite.push(testHTML);
 }
 
 console.log(componentsListMinusDSstore);
 
 
 
-
-//function writePatternCodeBlock(){
-function patternLibFunction(){
-	//for demo purposes
-	var nodeListOftheSections = document.querySelectorAll('.patternLib__section');
-	for(let i=0; i < nodeListOftheSections.length; i++){
-		let elementToAdd = document.createElement('div');
-		elementToAdd.classList.add('patternLib__codeBlock');
-		let cleanedInnerHTMLOfTheSection = nodeListOftheSections[i].innerHTML.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
-		let wrappedCleanHTML = `<pre><code>${cleanedInnerHTMLOfTheSection}</code></pre>`
-		elementToAdd.innerHTML = wrappedCleanHTML;
-		nodeListOftheSections[i].appendChild(elementToAdd);
-	}
-}
-
-document.addEventListener('DOMContentLoaded', patternLibFunction)
-
-}
+//It shouldn't build this page -- it should create a config array
+//that way i don't have to hack on templating here
 
 
-fs.writeFile('src/views/pages/_patternLib_precompiled.hbs', ObjectOfHTMLtoWrite);
+//add patternLibrScriptHere
+// var patternLibScript = '<script src=""></script>'
+// ObjectOfHTMLtoWrite.push(patternLibScript);
+// var patternLibScriptLiteral = 
+
+//pseudocode -- create script tag -- fill script tag -- append script tag
+
+
+
+// function writePatternCodeBlock(){
+// 	//for demo purposes
+// 	var nodeListOftheSections = document.querySelectorAll('.patternLib__section');
+// 	for(let i=0; i < nodeListOftheSections.length; i++){
+// 		let elementToAdd = document.createElement('div');
+// 		elementToAdd.classList.add('patternLib__codeBlock');
+// 		let cleanedInnerHTMLOfTheSection = nodeListOftheSections[i].innerHTML.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+// 		let wrappedCleanHTML = `<pre><code>${cleanedInnerHTMLOfTheSection}</code></pre>`
+// 		elementToAdd.innerHTML = wrappedCleanHTML;
+// 		nodeListOftheSections[i].appendChild(elementToAdd);
+// 	}
+// }
+
+// document.addEventListener('DOMContentLoaded', writePatternCodeBlock);
+
+var rejoinedObjectOfHTMLtoWrite = ObjectOfHTMLtoWrite.join('');
+
+fs.writeFile('src/core/__patternLib/patternLib_compiledList.hbs', rejoinedObjectOfHTMLtoWrite);
+
+
