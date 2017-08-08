5cbf783137545ba7ccda4758483b81b084677cf6
jB <jeremy.bates@gmail.com>
2017-08-08 07:36:18 -0400

    lazy loading adjustment

--- src/components/videoData/videoData.js
+++ src/components/videoData/videoData.js
@@ -1,24 +1,24 @@
 (function videoDataFunction(){
 
-	let testID;
-	let testSRC;
-	let testContent;
+	let testID = '999';
+	let testTitle = 'Test Title'
+	let testSRC = '';
+	let testContent = 'this is test content';
 	
 	var recordTemplate = 
 		`<section class="vidRecord">
 			<figure>
-				<img class="b-lazy" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="https://placehold.it/1024x1024" data-src="${testSRC}"/>
+				<img class="b-lazy" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="${testSRC}"/>
 				<figcaption>Video ID: ${testID}</figcaption>
 			</figure>
-			<h6></h6>
+			<h6>${testTitle}</h6>
 			<p>${testContent}</p>
 		</section>`
 	;
 
-
 	const apiURL = 'https://dev.core.bantr.wow.public.aol.com/video/getFeatured';
 
-	fetch(apiURL, {mode: 'cors'})  
+	fetch(apiURL)  
 	.then(function(response) { 
 		return response.json();
 	})
@@ -29,6 +29,7 @@
 		console.log('Request failed', error)  
 	});
 
+
 	function videoDataHandlerFunction(data){
 		//the "response" word here is actually from the data
 		data.response.map(function(record){//map the records to functions
@@ -36,14 +37,10 @@
 			console.log(recordTemplate)
 		})
 	}
-	//
 
-
-
-
+	//next steps 
 	// append fragments together before editing the DOM
-
-	// once markup is there, attach listeners (or use Vue)
-
+	// attach one listener to containing object -- then just get the target
+	// modal -- video element
 
 })();
